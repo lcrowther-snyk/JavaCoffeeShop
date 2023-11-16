cd4677c9ece14110951af4c79e8db1eedeaccfcc
lcrowther-snyk <lawrence.crowther@snyk.io>
2023-11-16 10:16:24 +1000

    added upload profile pic

--- src/main/java/org/workshop/coffee/controller/UploadController.java
+++ src/main/java/org/workshop/coffee/controller/UploadController.java
@@ -31,6 +31,30 @@ public class UploadController {
 
     @PostMapping("/uploadimage")
     public String uploadImage(Model model, @RequestParam("image") MultipartFile file, Principal principal) throws IOException {
+        //get the file name and save it to the UPLOAD_DIRECTORY
+        var fileName = file.getOriginalFilename();
+        //get path with file separator
+        var path = Paths.get(UPLOAD_DIRECTORY + File.separator + fileName);
+        //save file to path
+
+        //validate that there is no path traversal using the normalize method
+        if (!path.normalize().startsWith(UPLOAD_DIRECTORY)) {
+            //send "msg" to view
+            model.addAttribute("msg", "Invalid path!");
+            return "person/upload";
+        }
+
+
+
+        Files.write(path, file.getBytes());
+        //get person
+        var person = getPerson(model, principal);
+        //set image name
+        person.setProfilePicture(fileName);
+        //save person
+        personService.savePerson(person);
+        //add "msg"
+        model.addAttribute("msg", "Image uploaded successfully!");
 
         return "person/upload";
     }
