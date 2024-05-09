7d48f2991aa50df8a5e14fb52cf4c0a52149e2ca
lcrowther-snyk <lawrence.crowther@snyk.io>
2024-05-09 13:16:06 +1000

    added search feature

--- src/main/java/org/workshop/coffee/controller/HomeController.java
+++ src/main/java/org/workshop/coffee/controller/HomeController.java
@@ -41,6 +41,11 @@ public class HomeController {
         return "index";
     }
     public List<Product> searchProduct (String input) {
-        return null;
+        //create sql query for product_name OR description
+        var query = "SELECT * FROM product WHERE product_name LIKE '%" + input + "%' OR description LIKE '%" + input + "%'";
+        //execute query
+        return em.createNativeQuery(query, Product.class).getResultList();
+
+
     }
 }
