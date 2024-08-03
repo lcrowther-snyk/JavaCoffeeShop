720ae683d297294719ead5896712db089df04cb3
lawrencecrowther <loz@snyk.io>
2024-08-03 16:14:21 +0900

    added search function

--- src/main/java/org/workshop/coffee/controller/HomeController.java
+++ src/main/java/org/workshop/coffee/controller/HomeController.java
@@ -41,6 +41,10 @@ public class HomeController {
         return "index";
     }
     public List<Product> searchProduct (String input) {
-        return null;
+        //create sql query
+        String sql = "SELECT * FROM product WHERE product_name LIKE '%" + input + "%'";
+        //execute query
+        List<Product> products = em.createNativeQuery(sql, Product.class).getResultList();
+        return products;
     }
 }
